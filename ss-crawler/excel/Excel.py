a4a085afa58560481ef4022b74c95907902aa27e
haZuny <gkwns5791@naver.com>
2024-06-11 11:51:29 +0900

    [feat] 엑셀 row 간격 조절 기능 추가
    엑셀에 저장되는 row 항목들의 간격을 조절하는 기능을 추가함

--- ss-crawler/excel/Excel.py
+++ ss-crawler/excel/Excel.py
@@ -15,12 +15,18 @@ class Excel:
 
     
     ### insert full data
-    def insertData(self, row, column, values):
+    def insertData(self, row, column, rowGap, values):
         for i in range(len(values)):
             for j in range(len(values[i])):
-                self.ws.cell(row+i, column+j, values[i][j])
+                self.ws.cell(row+ rowGap*i, column+j, values[i][j])
     
     
     ### save
     def save(self):
         self.wb.save(self.fileName)
+
+    
+    ### create excel file and insert data
+    def createAndInsertData(self, row, column, rowGap, values):
+        self.insertData(row, column, rowGap, values)
+        self.save()
--- ss-crawler/main.py
+++ ss-crawler/main.py
@@ -2,7 +2,17 @@ from cralwer import HyunstorymallCralwer
 from excel import Excel
 
 if __name__ == '__main__':
-    # a = HyunstorymallCralwer.HyunstorymallCralwer()
-    # a.getProductsUntilLastReview(a.driver)
+    a = HyunstorymallCralwer.HyunstorymallCralwer()
+    data = a.getProductsUntilLastReview(a.driver)
+    dataSize = len(data['names'])
 
-    e = Excel.Excel()
+    values = []
+    for i in range(dataSize):
+        row = [data['names'][i], data['prices'][i]]
+        values.append(row)
+
+
+    e = Excel.Excel("C:/Users/gkwns/Hajun/smart-store-crawling/test.xlsx")
+    e.createAndInsertData(2, 2, 3, values)
+    print('end')
+
